77f2390764679fcb69ffaeb7a45b6ea219b5d46c
smdn <smdn@smdn.jp>
2022-08-17 00:28:40 +0900

    use ES2022 features

--- src/impls/javascript/README.md
+++ src/impls/javascript/README.md
@@ -1,7 +1,7 @@
 [二分木を使った数式の逆ポーランド記法化と計算](https://smdn.jp/programming/tips/polish/)のJavaScriptでの実装。
 
 # 前提条件・依存関係
-[Node.js](https://nodejs.org/ja/)が必要です。
+[Node.js](https://nodejs.org/ja/) v16以降が必要です。　ブラウザで実行する場合はES2022以降に対応している必要があります。
 
 # ビルドおよび実行
 `polish.js`を直接実行するか、コマンド`node polish.js`で実行できます。
@@ -17,8 +17,9 @@ polish notation: = x + 1 2
 calculated expression: (x = 3)
 ```
 
-## Node.jsのインストール方法
+## Node.js (v18)のインストール方法
 Ubuntuの場合:
 ```sh
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 sudo apt install nodejs
 ```
--- src/impls/javascript/polish.js
+++ src/impls/javascript/polish.js
@@ -6,12 +6,16 @@
 
 // ノードを構成するデータ構造
 class Node {
+  #expression;    // このノードが表す式(二分木への分割後は演算子または項となる)
+  #left = null;   // 左の子ノード
+  #right = null;  // 右の子ノード
+
+  get expression() { return this.#expression; }
+
   // コンストラクタ(与えられた式expressionを持つノードを構成する)
   constructor(expression)
   {
-    this.expression = expression; // このノードが表す式(二分木への分割後は演算子または項となる)
-    this.left = null;  // 左の子ノード
-    this.right = null; // 右の子ノード
+    this.#expression = expression;
   }
 
   // 式expression内の括弧の対応を検証するメソッド
@@ -49,41 +53,41 @@ class Node {
   parse()
   {
     // 式expressionから最も外側にある丸括弧を取り除く
-    this.expression = Node.removeOutermostBracket(this.expression);
+    this.#expression = Node.#removeOutermostBracket(this.#expression);
 
     // 式expressionから演算子を探して位置を取得する
-    let posOperator = Node.getOperatorPosition(this.expression);
+    let posOperator = Node.#getOperatorPosition(this.#expression);
 
     if (posOperator < 0) {
       // 式expressionに演算子が含まれない場合、expressionは項であるとみなす
       // (左右に子ノードを持たないノードとする)
-      this.left = null;
-      this.right = null;
+      this.#left = null;
+      this.#right = null;
       return;
     }
 
-    if (posOperator === 0 || posOperator === this.expression.length - 1)
+    if (posOperator === 0 || posOperator === this.#expression.length - 1)
       // 演算子の位置が式の先頭または末尾の場合は不正な式とする
-      throw "invalid expression: " + this.expression;
+      throw "invalid expression: " + this.#expression;
 
     // 以下、演算子の位置をもとに左右の部分式に分割する
 
     // 演算子の左側を左の部分式としてノードを作成する
-    this.left = new Node(this.expression.substr(0, posOperator));
+    this.#left = new Node(this.#expression.substr(0, posOperator));
     // 左側のノード(部分式)について、再帰的に二分木へと分割する
-    this.left.parse();
+    this.#left.parse();
 
     // 演算子の右側を右の部分式としてノードを作成する
-    this.right = new Node(this.expression.substr(posOperator + 1));
+    this.#right = new Node(this.#expression.substr(posOperator + 1));
     // 右側のノード(部分式)について、再帰的に二分木へと分割する
-    this.right.parse();
+    this.#right.parse();
 
     // 残った演算子部分をこのノードに設定する
-    this.expression = this.expression.substr(posOperator, 1);
+    this.#expression = this.#expression.at(posOperator);
   }
 
   // 式expressionから最も外側にある丸括弧を取り除いて返すメソッド
-  static removeOutermostBracket(expression)
+  static #removeOutermostBracket(expression)
   {
     let hasOutermostBracket = false; // 最も外側に括弧を持つかどうか
     let nest = 0; // 丸括弧の深度(式中で開かれた括弧が閉じられたかどうか調べるために用いる)
@@ -126,13 +130,13 @@ class Node {
       throw "empty bracket: " + expression;
 
     // 最初と最後の文字を取り除く(最も外側の丸括弧を取り除く)
-    expression = expression.substr(1, expression.length - 2);
+    expression = expression.slice(1, -1);
 
     // 取り除いた後の文字列の最も外側に括弧が残っている場合
     // 例:"((1+2))"などの場合
-    if (expression[0] === '(' && expression[expression.length - 1] === ')')
+    if (expression.at(0) === '(' && expression.at(-1) === ')')
       // 再帰的に呼び出して取り除く
-      expression = Node.removeOutermostBracket(expression);
+      expression = Node.#removeOutermostBracket(expression);
 
     // 取り除いた結果を返す
     return expression;
@@ -140,7 +144,7 @@ class Node {
 
   // 式expressionから最も右側にあり、かつ優先順位が低い演算子を探して位置を返すメソッド
   // (演算子がない場合は-1を返す)
-  static getOperatorPosition(expression)
+  static #getOperatorPosition(expression)
   {
     let posOperator = -1; // 現在見つかっている演算子の位置(初期値として-1=演算子なしを設定)
     let currentPriority = Number.MAX_VALUE; // 現在見つかっている演算子の優先順位(初期値としてNumber.MAX_VALUEを設定)
@@ -183,14 +187,14 @@ class Node {
   traversePostorder(stdout)
   {
     // 左右に子ノードをもつ場合、表示する前にノードを再帰的に巡回する
-    if (this.left)
-      this.left.traversePostorder(stdout);
-    if (this.right)
-      this.right.traversePostorder(stdout);
+    if (this.#left)
+      this.#left.traversePostorder(stdout);
+    if (this.#right)
+      this.#right.traversePostorder(stdout);
 
     // 巡回を終えた後でノードの演算子または項を表示する
     // (読みやすさのために項の後に空白を補って表示する)
-    stdout.write(this.expression + " ");
+    stdout.write(this.#expression + " ");
   }
 
   // 中間順序訪問(通りがけ順)で二分木を巡回して
@@ -198,30 +202,30 @@ class Node {
   traverseInorder(stdout)
   {
     // 左右に項を持つ場合、読みやすさのために項の前に開き括弧を補う
-    if (this.left && this.right)
+    if (this.#left && this.#right)
       stdout.write("(");
 
     // 表示する前に左の子ノードを再帰的に巡回する
-    if (this.left) {
-      this.left.traverseInorder(stdout);
+    if (this.#left) {
+      this.#left.traverseInorder(stdout);
 
       // 読みやすさのために空白を補う
       stdout.write(" ");
     }
 
     // 左の子ノードの巡回を終えた後でノードの演算子または項を表示する
-    stdout.write(this.expression);
+    stdout.write(this.#expression);
 
     // 表示した後に右の子ノードを再帰的に巡回する
-    if (this.right) {
+    if (this.#right) {
       // 読みやすさのために空白を補う
       stdout.write(" ");
 
-      this.right.traverseInorder(stdout);
+      this.#right.traverseInorder(stdout);
     }
 
     // 左右に項を持つ場合、読みやすさのために項の後に閉じ括弧を補う
-    if (this.left && this.right)
+    if (this.#left && this.#right)
       stdout.write(")");
   }
 
@@ -231,13 +235,13 @@ class Node {
   {
     // 巡回を始める前にノードの演算子または項を表示する
     // (読みやすさのために項の後に空白を補って表示する)
-    stdout.write(this.expression + " ");
+    stdout.write(this.#expression + " ");
 
     // 左右に子ノードをもつ場合、表示した後にノードを再帰的に巡回する
-    if (this.left)
-      this.left.traversePreorder(stdout);
-    if (this.right)
-      this.right.traversePreorder(stdout);
+    if (this.#left)
+      this.#left.traversePreorder(stdout);
+    if (this.#right)
+      this.#right.traversePreorder(stdout);
   }
 
   // 現在のノードの演算子と左右の子ノードの値から、ノードの値を計算するメソッド
@@ -247,21 +251,21 @@ class Node {
   {
     // 左右に子ノードを持たない場合、現在のノードは部分式ではなく項であり、
     // それ以上計算できないのでtrueを返す
-    if (!this.left || !this.right)
+    if (!this.#left || !this.#right)
       return true;
 
     // 左右の子ノードについて、再帰的にノードの値を計算する
-    this.left.calculate();
-    this.right.calculate();
+    this.#left.calculate();
+    this.#right.calculate();
 
     // 計算した左右の子ノードの値を数値型(Number)に変換する
     // 変換できない場合(左右の子ノードが記号を含む式などの場合)は、
     // ノードの値が計算できないものとして、falseを返す
 
     // 左ノードの値を数値に変換して演算子の左項leftOperandの値とする
-    let leftOperand  = new Number( this.left.expression);
+    let leftOperand = new Number(this.#left.expression);
     // 右ノードの値を数値に変換して演算子の右項rightOperandの値とする
-    let rightOperand = new Number(this.right.expression);
+    let rightOperand = new Number(this.#right.expression);
 
     if (isNaN(leftOperand) || isNaN(rightOperand))
       // Numberで扱える範囲外の値か、途中に変換できない文字があるため、計算できないものとして扱う
@@ -269,26 +273,26 @@ class Node {
 
     // 現在のノードの演算子に応じて左右の子ノードの値を演算し、
     // 演算した結果を文字列に変換して再度expressionに代入することで現在のノードの値とする
-    switch (this.expression[0]) {
-      case "+": this.expression = this.formatNumber(leftOperand + rightOperand); break;
-      case "-": this.expression = this.formatNumber(leftOperand - rightOperand); break;
-      case "*": this.expression = this.formatNumber(leftOperand * rightOperand); break;
-      case "/": this.expression = this.formatNumber(leftOperand / rightOperand); break;
+    switch (this.#expression[0]) {
+      case "+": this.#expression = this.#formatNumber(leftOperand + rightOperand); break;
+      case "-": this.#expression = this.#formatNumber(leftOperand - rightOperand); break;
+      case "*": this.#expression = this.#formatNumber(leftOperand * rightOperand); break;
+      case "/": this.#expression = this.#formatNumber(leftOperand / rightOperand); break;
       // 上記以外の演算子の場合は計算できないものとして、falseを返す
       default: return false;
     }
 
     // 左右の子ノードの値から現在のノードの値が求まったため、
     // このノードは左右に子ノードを持たない値のみのノードとする
-    this.left = null;
-    this.right = null;
+    this.#left = null;
+    this.#right = null;
 
     // 計算できたため、trueを返す
     return true;
   }
 
   // 演算結果の数値を文字列化するためのメソッド
-  formatNumber(number)
+  #formatNumber(number)
   {
     let nf = new Intl.NumberFormat("en", {
       style: "decimal",
@@ -331,7 +335,7 @@ function polish_main(_expression) {
     process.exit(1);
 
   // 入力された式から空白を除去する(空白を空の文字列に置き換える)
-  let expression = _expression.replace(/\s+/g, "");
+  let expression = _expression.replaceAll(" ", "");
 
   if (expression.length == 0)
     // 空白を除去した結果、空の文字列となった場合は、処理を終了する
