0fa51fceba21d8b7c0643e61c4dd5f30da91e1f1
smdn <smdn@smdn.jp>
2022-09-04 21:10:57 +0900

    add test cases

--- tests/demo/run-test.ps1
+++ tests/demo/run-test.ps1
@@ -2,37 +2,72 @@
 # SPDX-FileCopyrightText: 2022 smdn <smdn@smdn.jp>
 # SPDX-License-Identifier: MIT
 
-$input_expression = "2 + 5 * 3 - 4"
 $test_output_dir = "test-output"
+$testcases = @()
 
-$testcases = (
+$input_expression_calculable = "2 + 5 * 3 - 4"
+
+$testcases += (
   [PSCustomObject]@{
     "VisualizationMode" = "traverse-preorder"
-    "InputExpression" = $input_expression
+    "InputExpression" = $input_expression_calculable
     "OutputSvgFileName" = "calculable.traverse-preorder.generated.svg"
     "ExpectedSvgFileName" = "calculable.traverse-preorder.expected.svg"
     "OutputDiffFileName" = "calculable.traverse-preorder.svg.diff"
   },
   [PSCustomObject]@{
     "VisualizationMode" = "traverse-inorder"
-    "InputExpression" = $input_expression
+    "InputExpression" = $input_expression_calculable
     "OutputSvgFileName" = "calculable.traverse-inorder.generated.svg"
     "ExpectedSvgFileName" = "calculable.traverse-inorder.expected.svg"
     "OutputDiffFileName" = "calculable.traverse-inorder.svg.diff"
   },
   [PSCustomObject]@{
     "VisualizationMode" = "traverse-postorder"
-    "InputExpression" = $input_expression
+    "InputExpression" = $input_expression_calculable
     "OutputSvgFileName" = "calculable.traverse-postorder.generated.svg"
     "ExpectedSvgFileName" = "calculable.traverse-postorder.expected.svg"
     "OutputDiffFileName" = "calculable.traverse-postorder.svg.diff"
   },
   [PSCustomObject]@{
     "VisualizationMode" = "calculate"
-    "InputExpression" = $input_expression
+    "InputExpression" = $input_expression_calculable
     "OutputSvgFileName" = "calculable.calculation.generated.svg"
     "ExpectedSvgFileName" = "calculable.calculation.expected.svg"
     "OutputDiffFileName" = "calculable.calculation.svg.diff"
+  }
+)
+
+$input_expression_incalculable = "1 + 2 + X = Y + 3 + 4"
+
+$testcases += (
+  [PSCustomObject]@{
+    "VisualizationMode" = "traverse-preorder"
+    "InputExpression" = $input_expression_incalculable
+    "OutputSvgFileName" = "incalculable.traverse-preorder.generated.svg"
+    "ExpectedSvgFileName" = "incalculable.traverse-preorder.expected.svg"
+    "OutputDiffFileName" = "incalculable.traverse-preorder.svg.diff"
+  },
+  [PSCustomObject]@{
+    "VisualizationMode" = "traverse-inorder"
+    "InputExpression" = $input_expression_incalculable
+    "OutputSvgFileName" = "incalculable.traverse-inorder.generated.svg"
+    "ExpectedSvgFileName" = "incalculable.traverse-inorder.expected.svg"
+    "OutputDiffFileName" = "incalculable.traverse-inorder.svg.diff"
+  },
+  [PSCustomObject]@{
+    "VisualizationMode" = "traverse-postorder"
+    "InputExpression" = $input_expression_incalculable
+    "OutputSvgFileName" = "incalculable.traverse-postorder.generated.svg"
+    "ExpectedSvgFileName" = "incalculable.traverse-postorder.expected.svg"
+    "OutputDiffFileName" = "incalculable.traverse-postorder.svg.diff"
+  },
+  [PSCustomObject]@{
+    "VisualizationMode" = "calculate"
+    "InputExpression" = $input_expression_incalculable
+    "OutputSvgFileName" = "incalculable.calculation.generated.svg"
+    "ExpectedSvgFileName" = "incalculable.calculation.expected.svg"
+    "OutputDiffFileName" = "incalculable.calculation.svg.diff"
   }
 )
 
