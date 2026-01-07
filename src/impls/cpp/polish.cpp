e81812c4436f4d3f20f1d48c13bf07eb6263483f
smdn <smdn@smdn.jp>
2026-01-07 23:11:41 +0900

    include <charconv> to use std::from_chars

--- src/impls/cpp/polish.cpp
+++ src/impls/cpp/polish.cpp
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: 2022 smdn <smdn@smdn.jp>
 // SPDX-License-Identifier: MIT
 #include <algorithm>
+#include <charconv>
 #include <format>
 #include <functional>
 #include <iostream>
